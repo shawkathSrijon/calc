b5c86a6d64c7514b2689a41db6a9ac06ee37fd4b
Habibur Rahman <habiburrahman@habiburs-air>
2018-04-03 21:33:24 +1000

    New 'Validation' file has been created

---
+++ calc/Validation.swift
@@ -0,0 +1,23 @@
+//
+//  Validation.swift
+//  calc
+//
+//  Created by Habibur Rahman on 3/4/18.
+//  Copyright © 2018 UTS. All rights reserved.
+//
+
+import Foundation
+
+class Validation {
+    var firstArg : Int
+    var lastArg : Int
+    
+    init(firstArg : Int, lastArg : Int) {
+        self.firstArg = firstArg
+        self.lastArg = lastArg
+    }
+    
+    func validateInputExpression(firstArg : Int, lastArg : Int) -> String {
+        if firstArg.
+    }
+}
--- calc/main.swift
+++ calc/main.swift
@@ -8,7 +8,11 @@
 
 import Foundation
 
-var args = ProcessInfo.processInfo.arguments
+//take commend-line arguments into an array called args[]
+var args: [String] = ProcessInfo.processInfo.arguments
 args.removeFirst() // remove the name of the program
 
-print(Int(args[0])!)
+var lastArgument : Int = args.endIndex
+print("the arguments are \(args[lastArgument - 1])")
+
+if args[lastArgument - 1] == 
